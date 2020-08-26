8a76ffe793c81090bf91744b6d720104a125bb33
Lucas Moura <lsantosdemoura99@gmail.com>
2020-08-26 17:27:18 -0300

    refactor: Removing queues from service and changing some tests.

--- tests/test_viacep.py
+++ tests/test_viacep.py
@@ -1,8 +1,7 @@
-from multiprocessing import Process, Queue
-
-import pytest
+import vcr
 
 from cep_address.services import viacep
+
 
 def test_parse_address_valid_cep(
     valid_viacep_response, formatted_valid_cep, valid_viacep_service_return
@@ -38,15 +37,15 @@ def test_parse_address_invalid_cep(invalid_cep, formatted_invalid_cep):
     )
 
 
-# def test_get_address(
-#     valid_viacep_response, formatted_valid_cep, valid_viacep_service_return, mocker
-# ):
-#     q = Queue()
-#     p = Process()
-#     p.start()
-#     mock_request = mocker.patch("cep_address.services.viacep.request")
-#     mock_request.json.return_value = valid_viacep_service_return
-#     viacep.get_address(queue=q, cep=formatted_valid_cep)
-#     response = q.get(timeout=1)
-#     breakpoint()
-#     p.join()
+@vcr.use_cassette('tests/fixtures/vcr_cassettes/viacep_valid_cep.yml')
+def test_request_valid_cep(formatted_valid_cep, valid_viacep_response):
+    response = viacep.request(formatted_valid_cep)
+    assert response.status_code == 200
+    assert response.json() == valid_viacep_response
+
+
+@vcr.use_cassette('tests/fixtures/vcr_cassettes/viacep_invalid_cep.yml')
+def test_request_invalid_cep(invalid_cep, invalid_viacep_response):
+    response = viacep.request(invalid_cep)
+    assert response.status_code == 200
+    assert response.json() == invalid_viacep_response
